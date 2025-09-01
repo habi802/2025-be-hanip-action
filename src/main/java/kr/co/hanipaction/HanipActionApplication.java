64e65a1ed1fe69bd2288e6e3f0f99539646c9f79
habi802 <shb8002@gmail.com>
2025-09-01 11:57:58 +0900

    [init] first

---
+++ src/main/java/kr/co/hanipaction/HanipActionApplication.java
@@ -0,0 +1,13 @@
+package kr.co.hanipaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanipActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanipActionApplication.class, args);
+    }
+
+}
