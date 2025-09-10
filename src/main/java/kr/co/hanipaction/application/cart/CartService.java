f2c0aafea7565fe49758f2d67f48a1df3cfc77c7
djinylh <d.jinylh@gmail.com>
2025-09-10 18:34:17 +0900

    modify save 메소드로 리턴 시킴

--- src/main/java/kr/co/hanipaction/application/cart/CartService.java
+++ src/main/java/kr/co/hanipaction/application/cart/CartService.java
@@ -117,78 +117,8 @@ public class CartService {
 
         cartRepository.delete(cartPk);
 
-            MenuGetReq menuGetReq = new MenuGetReq();
-            menuGetReq.setMenuIds(Collections.singletonList((req.getMenuId())));
-            menuGetReq.setOptionIds(req.getOptionId());
 
-            ResultResponse<List<MenuGetRes>>  menuRes = menuClient.getOrderMenu(menuGetReq);
-
-
-            if (menuRes == null || menuRes.getResult() == null) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "메뉴 정보를 호출 할 수 없습니다.");
-            }
-            MenuGetRes menu = menuRes.getResult().get(0);
-
-            int menuPrice = menu.getPrice();
-
-            int totalOptionPrice = 0;
-            List<Long> selectedOptionIds = req.getOptionId();
-
-
-            for(MenuGetRes.Option option : menu.getOptions()){
-
-                if(selectedOptionIds.contains(option.getOptionId())) {
-                    totalOptionPrice += option.getPrice();
-                }
-                if(option.getChildren() != null){
-                    for(MenuGetRes.Option child : option.getChildren()){
-                        if(selectedOptionIds.contains(child.getOptionId())) {
-                            totalOptionPrice += child.getPrice();
-                        }
-                    }
-                }
-
-            }
-            int totalAmount =  (menuPrice + totalOptionPrice) * req.getQuantity();
-
-            List<MenuGetRes> menuList = menuRes.getResult();
-
-            Map<Long, String> optionIdToCommentMap = new HashMap<>();
-            Map<Long, Integer> optionIdToPriceMap = new HashMap<>();
-            Map<Long, Long> optionIdToParentIdMap = new HashMap<>();
-
-            for (MenuGetRes menus : menuList) {
-                for (MenuGetRes.Option option : menu.getOptions()) {
-                    collectOptions(option, optionIdToCommentMap, optionIdToPriceMap, optionIdToParentIdMap);
-                }
-            }
-
-
-            Cart cart = Cart.builder()
-                    .menuId(req.getMenuId())
-                    .menuName(menu.getName())
-                    .userId(userId)
-                    .storeId(req.getStoreId())
-                    .amount(totalAmount)
-                    .quantity(req.getQuantity())
-                    .imgPath(menu.getImagePath())
-                    .build();
-
-            // cart 메뉴에 따라 옵션 테이블 들어감
-            List<CartMenuOption> options = req.getOptionId().stream()
-                    .map(optionId -> CartMenuOption.builder()
-                            .menuId(cart)
-                            .optionName(optionIdToCommentMap.get(optionId))
-                            .optionPrice(optionIdToPriceMap.get(optionId))
-                            .optionId(optionId)
-                            .parentId(optionIdToParentIdMap.get(optionId))
-                            .build())
-                    .collect(Collectors.toList());
-
-
-            cart.setOptions(options);
-
-            return cartRepository.save(cart);
+        return save(req, userId);
 
 
     }
