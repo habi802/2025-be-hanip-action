e370a4305f5141b3b5a5705ec348bc26dbb700df
habi802 <shb8002@gmail.com>
2025-09-12 13:02:02 +0900

    [feat] 관리자 - 주문 조회 작업중

--- src/main/java/kr/co/hanipaction/application/manager/ManagerService.java
+++ src/main/java/kr/co/hanipaction/application/manager/ManagerService.java
@@ -1,5 +1,6 @@
 package kr.co.hanipaction.application.manager;
 
+import kr.co.hanipaction.application.common.model.ResultResponse;
 import kr.co.hanipaction.application.manager.model.*;
 import kr.co.hanipaction.application.manager.specification.OrderSpecification;
 import kr.co.hanipaction.application.order.OrderRepository;
@@ -9,15 +10,19 @@ import kr.co.hanipaction.entity.Orders;
 import kr.co.hanipaction.entity.Review;
 import kr.co.hanipaction.entity.ReviewImage;
 import kr.co.hanipaction.openfeign.store.StoreClient;
+import kr.co.hanipaction.openfeign.store.model.StoreInManagerRes;
 import kr.co.hanipaction.openfeign.store.model.StoreListReq;
+import kr.co.hanipaction.openfeign.store.model.StoreListRes;
 import kr.co.hanipaction.openfeign.user.UserClient;
 import kr.co.hanipaction.openfeign.user.model.UserListReq;
+import kr.co.hanipaction.openfeign.user.model.UserListRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,11 +42,11 @@ public class ManagerService {
     // 주문 전체 조회
     public Page<OrderListRes> getOrderList(OrderListReq req) {
         // 주문자명, 상호명 같이 외부 api를 호출해야 하는 경우 입력한 주문자명 혹은 상호명을 가진 id 리스트를 검색 조건으로 사용
-        List<Long> userIds = new ArrayList<>();
-        userClient.getUserIdsInManager(UserListReq.builder().name(req.getUserName()).build());
+        ResponseEntity<ResultResponse<Page<UserListRes>>> userListRes = userClient.getUserIdsInManager(UserListReq.builder().name(req.getUserName()).build());
+        List<Long> userIds = userListRes.getBody().getResultData().stream().map(UserListRes::getUserId).toList();
 
-        List<Long> storeIds = new ArrayList<>();
-        storeClient.getStoreIdsInManager(StoreListReq.builder().name(req.getStoreName()).build());
+        ResponseEntity<ResultResponse<Page<StoreListRes>>> storeListRes = storeClient.getStoreIdsInManager(StoreListReq.builder().name(req.getStoreName()).build());
+        List<Long> storeIds = storeListRes.getBody().getResultData().stream().map(StoreListRes::getStoreId).toList();
 
         // 검색 조건 적용
         Specification<Orders> spec = OrderSpecification.hasStartDate(req.getStartDate())
@@ -57,9 +62,22 @@ public class ManagerService {
         Pageable pageable = PageRequest.of(req.getPageNumber(), req.getPageSize());
 
         Page<Orders> page = orderRepository.findAll(spec, pageable);
-        Page<OrderListRes> result = page.map(order -> OrderListRes.builder()
+        Page<OrderListRes> result = page.map(order -> {
+            // 작성자명, 상호명 가져오기 위해 외부 api 호출
+            ResponseEntity<ResultResponse<String>> userRes = userClient.getUserNameInManager(order.getUserId());
+            ResponseEntity<ResultResponse<StoreInManagerRes>> storeRes = storeClient.getStoreNameInManager(order.getStoreId());
 
-                .build());
+            return OrderListRes.builder()
+                               //.createdAt()
+                               .orderId(order.getId())
+                               .userName(userRes.getBody().getResultData())
+                               .storeName(storeRes.getBody().getResultData().getName())
+                               .address(String.format("%s, %s, %s", order.getPostcode(), order.getAddress(), order.getAddressDetail()))
+                               .payment(order.getPayment().getValue())
+                               .status(order.getStatus().getValue())
+                               .isDeleted(order.getIsDeleted())
+                               .build();
+        });
 
         return result;
     }
@@ -68,13 +86,17 @@ public class ManagerService {
     public OrderInManagerRes getOrder(Long orderId) {
         Orders order = orderRepository.findById(orderId).orElse(null);
 
+        // 작성자명, 상호명 가져오기 위해 외부 api 호출
+        ResponseEntity<ResultResponse<String>> userRes = userClient.getUserNameInManager(order.getUserId());
+        ResponseEntity<ResultResponse<StoreInManagerRes>> storeRes = storeClient.getStoreNameInManager(order.getStoreId());
+
         return OrderInManagerRes.builder()
                                 .orderId(orderId)
-                                //.storeName()
-                                //.userName()
+                                .userName(userRes.getBody().getResultData())
+                                .storeName(storeRes.getBody().getResultData().getName())
                                 .address(String.format("%s, %s, %s", order.getPostcode(), order.getAddress(), order.getAddressDetail()))
-                                .payment(order.getPayment().getValue())
                                 .amount(order.getAmount())
+                                .payment(order.getPayment().getValue())
                                 .status(order.getStatus().getValue())
                                 .isDeleted(order.getIsDeleted())
                                 .build();
@@ -94,6 +116,7 @@ public class ManagerService {
     public ReviewInManagerRes getReview(Long reviewId) {
         Review review = reviewRepository.findById(reviewId).orElse(null);
 
+        // 리뷰 이미지 가져오기
         List<ReviewImage> reviewImages = review.getReviewPicList();
         List<String> images = new ArrayList<>();
         for (ReviewImage image : reviewImages) {
