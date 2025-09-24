56c7447df9f2f0625977313e8d973280aa780f69
habi802 <shb8002@gmail.com>
2025-09-24 21:08:11 +0900

    [fix] 토큰 만료 관련 코드 추가 및 수정

--- src/main/java/kr/co/hanipaction/configuration/security/WebSecurityConfiguration.java
+++ src/main/java/kr/co/hanipaction/configuration/security/WebSecurityConfiguration.java
@@ -1,6 +1,6 @@
 package kr.co.hanipaction.configuration.security;
 
-
+import kr.co.hanipaction.configuration.enumcode.model.EnumUserRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 
 /*
 @Configuration - bean 등록, Bean 메소드가 있다.
@@ -20,6 +19,7 @@ Bean 메소드는 무조건 싱글톤으로 처리된다.
 public class WebSecurityConfiguration {
 
     private final UserHeaderAuthenticationFilter userHeaderAuthenticationFilter;
+    private final TokenAuthenticationEntryPoint tokenAuthenticationEntryPoint;
 
     @Bean //스프링이 메소드 호출을 하고 리턴한 객체의 주소값을 관리한다. (빈등록)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,7 +27,14 @@ public class WebSecurityConfiguration {
                 .httpBasic(h -> h.disable()) //SSR(Server Side Rendering)이 아니다. 화면을 만들지 않기 때문에 비활성화 시킨다. 시큐리티 로그인창 나타나지 않을 것이다.
                 .formLogin(form -> form.disable()) //SSR(Server Side Rendering)이 아니다. 폼로그인 기능 자체를 비활성화
                 .csrf(csrf -> csrf.disable()) //SSR(Server Side Rendering)이 아니다. 보안관련 SSR 이 아니면 보안이슈가 없기 때문에 기능을 끈다.
+                .authorizeHttpRequests(req -> req
+                        // 관리자 계정만 가능한 API
+                        .requestMatchers("/api/hanip-manager/action/**").hasRole(EnumUserRole.MANAGER.name())
+
+                        .requestMatchers("/api/order/rider").permitAll()
+                )
                 .addFilterBefore(userHeaderAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(e -> e.authenticationEntryPoint(tokenAuthenticationEntryPoint))
                 .build();
     }
 
