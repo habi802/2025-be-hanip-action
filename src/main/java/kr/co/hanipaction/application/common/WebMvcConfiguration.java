4f78bb54347b02150581eb5c8f16a321a2387d5d
djinylh <d.jinylh@gmail.com>
2025-10-10 13:05:40 +0900

    [docs] 주소 수정

--- src/main/java/kr/co/hanipaction/application/common/WebMvcConfiguration.java
+++ src/main/java/kr/co/hanipaction/application/common/WebMvcConfiguration.java
@@ -24,7 +24,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("/hanip/images/**", "/images/**")
+        registry.addResourceHandler("/hanip-api/images/**", "/images/**")
                 .addResourceLocations("file:" + constFile.getUploadDirectory());
 
         registry.addResourceHandler("/**")
